c486656ea03e405ef3d1a68591823e6a6dc127f6
Mercy Manrique <mercymanrique@yahoo.com>
2017-08-07 17:36:12 -0700

    updating index.js

--- index.js
+++ index.js
@@ -1,61 +1,141 @@
-$(document).ready(function(){
- var lat;
- var long;
- $.getJSON("http://ip-api.com/json",function(data2){
-  lat=data2.lat;
-  long= data2.lon;
-  var api =
-      'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=208582f6415477b23f27f050de854385'; 
- 
-  
- $.getJSON(api, function(data){
-  var fTemp;
-  var cTemp;
-  var kTemp;
-  var tempSwap=true;
-  var weatherType= 
- data.weather[0].description;
- kTemp = data.main.temp;
- var windSpeed= data.wind.speed;
- var city = data.name;
- 
- //Temperature in Kelvin
- fTemp = (kTemp)*(9/5)-459.67.toFixed(1);
- //Temperature in F
-  //City name
- cTemp = (kTemp-273).toFixed(1);
- 
- console.log(city);
- $("#city").html(city);
-  $("#weatherType").html(weatherType);
-$("#fTemp").html(fTemp + " &#8457;");
- $("#fTemp").click(function(){
-  if(tempSwap===false){
-   
-    $("#fTemp").html(fTemp + " &#8457;");
-   tempSwap=true;
+$(document).ready(function() {
+  getLocation();
+})
+
+function getLocation() {
+  var location;
+  $.ajax({
+    format: "jsonp",
+    dataType: "jsonp",
+    url: "http://ip-api.com/json",
+    success: function(data) {
+      location = (data.lat + "," + data.lon);
+      $("#weather-location").html(data.city + ", " + data.region);
+      getURL(location)
+    },
+    error: function() {
+      httpsLocation();
+    },
+    method: "GET"
+  });
+
+  function httpsLocation() {
+    if (navigator.geolocation) {
+      var location;
+      navigator.geolocation.getCurrentPosition(passLocation);
+    }
   }
-  else{
-   $("#fTemp").html(cTemp + "&#8451;");
-  
-   tempSwap=false;
+
+  function passLocation(position) {
+    location = position.coords.latitude + ", " + position.coords.longitude;
+    setCity(location);
+    getURL(location);
   }
- });
- windSpeed = (2.237*(windSpeed)).toFix(1);
- $("#windSpeed").html(windSpeed + " mph");
- if(fTemp>80){
-  
- }
+}
 
- if (fTemp>70){
-  $('body').css('background-image', 'url(https://www.freeimageslive.co.uk/files/images006/clouds_1.jpg)');
-  
- }
-});
- });
+function setCity(latLon) {
+  var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latLon + "&sensor=true";
+  url = url.replace(/\s/g, "");
+  $.ajax({
+    format: "json",
+    dataType: "json",
+    url: url,
+    success: function(data) {
+      $('#weather-location').html(data.results[0].address_components[2].long_name);
+    },
+    method: "GET"
+  });
+}
 
-});
+function getURL(location, tempSetting) {
+  var url = ("https://api.forecast.io/forecast/2b4b9e2d0c9c7ba61f588616d2967c9c/" + location);
+  //console.log(url);
+  getJson(url);
 
-<script type="text/javascript" src="index.js"></script>
+}
 
+function getJson(url) {
+  //console.log("started getJson with this url: " + url);
+
+  $.ajax({
+      format: "jsonp",
+      dataType: "jsonp",
+      url: url,
+      success: function(json) {
+        //console.log("great success");
+        $("#weather-current").html(Math.round(json.currently.temperature) + "°");
+        $("#weather-high").html("High: " + Math.round(json.daily.data[0].temperatureMax) + "°");
+        $("#weather-low").html("Low: " + Math.round(json.daily.data[0].temperatureMin) + "°");
+        setBackground(json.currently.icon);
+      }
+
+    })
+    .error(function(jqXHR, textStatus, errorThrown) {
+      alert("error: " + JSON.stringify(jqXHR));
+    })
+}
+
+$("#temp-type").on("click", function() {
+  var currentTemp = $("#weather-current").html().replace(/°/g, "");
+  var highTemp = $("#weather-high").html().replace(/°/g, "");
+  var lowTemp = $("#weather-low").html().replace(/°/g, "");
+  lowTemp = lowTemp.replace("Low: ", "");
+  highTemp = highTemp.replace("High: ", "");
+
+  if ($("#temp-type").html() == "Fahrenheit") {
+    $("#weather-current").html(toCelsius(currentTemp) + "°");
+    $("#weather-high").html("High: " + toCelsius(highTemp) + "°");
+    $("#weather-low").html("Low: " + toCelsius(lowTemp) + "°");
+    $("#temp-type").html("Celsius");
+
+  } else if ($("#temp-type").html() == "Celsius") {
+    $("#weather-current").html(toFahrenheit(currentTemp) + "°");
+    $("#weather-high").html("High: " + toFahrenheit(highTemp) + "°");
+    $("#weather-low").html("Low: " + toFahrenheit(lowTemp) + "°");
+    $("#temp-type").html("Fahrenheit");
+
+  }
+
+  function toCelsius(num) {
+    var newNum = (parseInt(num) - 32) * 5 / 9;
+    return Math.round(newNum);
+  }
+
+  function toFahrenheit(num) {
+    var newNum = (parseInt(num) * 9 / 5) + 32;
+    return Math.round(newNum);
+  }
+
+})
+
+function setBackground(weatherIcon) {
+  //console.log(weatherIcon);
+  switch (weatherIcon) {
+    case "clear-day":
+      document.getElementById("body").style.backgroundImage = 'url("http://feelgrafix.com/data_images/out/15/899301-sunny-day.jpg")';
+      break;
+    case "clear-night":
+      document.getElementById("body").style.backgroundImage = 'url("https://tcklusman.files.wordpress.com/2014/05/tumblr_static_dark-starry-night-sky-226736.jpg")';
+      break;
+    case "rain":
+      document.getElementById("body").style.backgroundImage = 'url("http://wearechange.org/wp-content/uploads/2015/03/1_See_It.jpg")';
+      break;
+    case "cloudy":
+      document.getElementById("body").style.backgroundImage = 'url("http://www.tripwire.com/state-of-security/wp-content/uploads/cache//shutterstock_106367810/4261234929.jpg")';
+      break;
+    case "partly-cloudy-day":
+      document.getElementById("body").style.backgroundImage = 'url("http://www.sturdyforcommonthings.com/wp-content/uploads/2013/03/wind_blowing.jpg")';
+      break;
+    case "partly-cloudy-night":
+      document.getElementById("body").style.backgroundImage = 'url("http://scienceblogs.com/startswithabang/files/2013/04/night-sky-stars.jpeg")';
+      break;
+    case "snow":
+      document.getElementById("body").style.backgroundImage = 'url("http://www.vancitybuzz.com/wp-content/uploads/2015/12/shutterstock_315123593-984x500.jpg")';
+      break;
+    default:
+      break;
+
+  }
+
+}
 
